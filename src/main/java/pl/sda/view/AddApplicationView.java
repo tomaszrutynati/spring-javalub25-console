61c16614c6dc3394ac5b9389c66e7996beeb7ac0
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2021-04-11 14:08:58 +0200

    add app view

---
+++ src/main/java/pl/sda/view/AddApplicationView.java
@@ -0,0 +1,30 @@
+package pl.sda.view;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import pl.sda.service.ApplicationService;
+
+import java.util.Scanner;
+
+@Component
+@AllArgsConstructor
+public class AddApplicationView {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AddApplicationView.class);
+
+    private ApplicationService applicationService;
+
+    public void displayOnConsole() {
+        Scanner scanner = new Scanner(System.in);
+        LOGGER.info("Podaj nazwę aplikacji:");
+        String name = scanner.nextLine();
+        LOGGER.info("Podaj producenta aplikacji:");
+        String producer = scanner.nextLine();
+        LOGGER.info("Podaj wersję aplikacji:");
+        String version = scanner.nextLine();
+
+        applicationService.addApplication(producer, name, version);
+    }
+}
