f7ed4418865b31c4d6d8cfad4687986134999d38
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2021-04-11 14:25:57 +0200

    main menu display in main

--- src/main/java/pl/sda/SpringTrainingJavalub25Application.java
+++ src/main/java/pl/sda/SpringTrainingJavalub25Application.java
@@ -2,14 +2,19 @@ package pl.sda;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.sda.view.MainMenu;
 
 @SpringBootApplication
 public class SpringTrainingJavalub25Application implements CommandLineRunner {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(SpringTrainingJavalub25Application.class);
+
+    @Autowired
+    private MainMenu mainMenu;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringTrainingJavalub25Application.class, args);
@@ -17,6 +22,6 @@ public class SpringTrainingJavalub25Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        LOGGER.info("Hello world from Spring Boot app");
+        mainMenu.displayOnConsole();
     }
 }
