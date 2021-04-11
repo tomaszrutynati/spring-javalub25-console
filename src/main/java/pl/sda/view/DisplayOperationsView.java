0284726d0cd999182b4530f2ff18a283ed695d1e
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2021-04-11 14:13:58 +0200

    display apps view

---
+++ src/main/java/pl/sda/view/DisplayOperationsView.java
@@ -0,0 +1,29 @@
+package pl.sda.view;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import pl.sda.repository.Application;
+import pl.sda.service.ApplicationService;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class DisplayOperationsView {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DisplayOperationsView.class);
+
+    private ApplicationService applicationService;
+
+    public void displayOnConsole() {
+        List<Application> applications = applicationService.getAll();
+
+        LOGGER.info("Id | Producent | Nazwa | Wersja");
+        applications.forEach(
+                app -> LOGGER.info(String.format("%d | %s | %s | %s",
+                        app.getId(), app.getProducer(), app.getName(), app.getVersion())));
+
+    }
+
+}
